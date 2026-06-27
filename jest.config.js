0f3ce9dd168f050d54c73251ad9da5f354bc535f
KaioVitorTerra <vitorterrakaio@gmmail.com>
2026-06-27 13:27:03 -0300

    Ajusta configuração do Jest para 6 testes

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'jest-expo',
+  testMatch: ['**/utils.test.js', '**/__tests__/sprint1.test.js'],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
   ],
